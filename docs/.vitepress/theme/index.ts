ad5a9380d1ed54089b09dec494fdda916611e7d9
zzapcho <doyoon201203@gmail.com>
2026-08-16 19:24:47 +0900

    Enable wiki search shortcuts and UI enhancements

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,12 @@
 import DefaultTheme from 'vitepress/theme'
+import type { Theme } from 'vitepress'
 import './custom.css'
+import './enhancements.css'
+import { installEnhancements } from './enhancements'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  enhanceApp({ router }) {
+    installEnhancements(router)
+  }
+} satisfies Theme
